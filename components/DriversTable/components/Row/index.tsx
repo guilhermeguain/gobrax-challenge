930eb519a1cdcc8b533350ab17697c2dfaad7537
Guilherme Guain <guilherme.guain@gmail.com>
2024-06-24 19:58:09 -0300

    chore: adds drivers table component

---
+++ components/DriversTable/components/Row/index.tsx
@@ -0,0 +1,50 @@
+import { useCallback, useMemo } from "react";
+import { Text } from "react-native";
+import { DataTable, Checkbox } from "react-native-paper";
+
+import { DriverProps } from "@/services/jsonServer/driver/types";
+
+import { useAppDispatch, useAppSelector } from "@/store";
+import { DriverActions } from "@/store/driver";
+
+export const Row = (driver: DriverProps) => {
+  const dispatch = useAppDispatch();
+  const { activeDriver } = useAppSelector((state) => state.driver);
+
+  const status = useMemo(
+    () => (activeDriver?.id === driver.id ? "checked" : "unchecked"),
+    [activeDriver]
+  );
+
+  const hasVehicles = driver.vehicles?.length;
+
+  const hasVehiclesText = hasVehicles ? "Sim" : "Não";
+
+  const handleDriverSelect = useCallback(
+    (driver: DriverProps) => {
+      const isSelected = activeDriver?.id === driver.id;
+      const updatedSelectedDriver = isSelected ? undefined : driver;
+
+      dispatch(DriverActions.setActiveDriver(updatedSelectedDriver));
+    },
+    [activeDriver, dispatch]
+  );
+
+  return (
+    <DataTable.Row key={driver.id}>
+      <DataTable.Cell>
+        <Checkbox status={status} onPress={() => handleDriverSelect(driver)} />
+        <Text>{driver.id}</Text>
+      </DataTable.Cell>
+      <DataTable.Cell>
+        <Text>{driver.name}</Text>
+      </DataTable.Cell>
+      <DataTable.Cell numeric>
+        <Text>{driver.document}</Text>
+      </DataTable.Cell>
+      <DataTable.Cell numeric>
+        <Text>{hasVehiclesText}</Text>
+      </DataTable.Cell>
+    </DataTable.Row>
+  );
+};
---
+++ components/DriversTable/index.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
+import { DataTable } from "react-native-paper";
+
+import { useAppSelector } from "@/store";
+
+import { Row } from "./components/Row";
+
+export const DriversTable = () => {
+  const { drivers } = useAppSelector((state) => state.driver);
+
+  const [page, setPage] = useState<number>(0);
+  const [numberOfItemsPerPageList] = useState([10]);
+  const [itemsPerPage, setItemsPerPage] = useState(numberOfItemsPerPageList[0]);
+
+  const from = page * itemsPerPage;
+  const to = Math.min((page + 1) * itemsPerPage, drivers.length);
+
+  useEffect(() => {
+    setPage(0);
+  }, [itemsPerPage]);
+
+  return (
+    <DataTable style={styles.table}>
+      <DataTable.Header>
+        <DataTable.Title>ID</DataTable.Title>
+        <DataTable.Title>Nome</DataTable.Title>
+        <DataTable.Title numeric>Documento</DataTable.Title>
+        <DataTable.Title numeric>Vínculo</DataTable.Title>
+      </DataTable.Header>
+
+      {drivers?.slice(from, to).map((driver) => (
+        <Row key={driver.id} {...driver} />
+      ))}
+
+      <DataTable.Pagination
+        page={page}
+        numberOfPages={Math.ceil(drivers.length / itemsPerPage)}
+        onPageChange={(page) => setPage(page)}
+        label={`${from + 1}-${to} of ${drivers.length}`}
+        numberOfItemsPerPageList={numberOfItemsPerPageList}
+        numberOfItemsPerPage={itemsPerPage}
+        onItemsPerPageChange={setItemsPerPage}
+        showFastPaginationControls
+        selectPageDropdownLabel={"Linhas por página"}
+      />
+    </DataTable>
+  );
+};
+
+export const styles = StyleSheet.create({
+  table: {
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#e7e0ec",
+  },
+});
