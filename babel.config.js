d472c6757d1ccf571f223105181ba290bb254af0
Guilherme Guain <guilherme.guain@gmail.com>
2024-06-25 21:43:20 -0300

    fix: fixes build error

--- babel.config.js
+++ babel.config.js
@@ -6,12 +6,6 @@ module.exports = function(api) {
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-      ['module-resolver', {
-        root: ['./'],
-        alias: {
-          '@': './',
-        },
-      }],
     ],
   };
 };
